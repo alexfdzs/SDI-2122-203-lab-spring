6d2e0a68d3f286af134d0941575a054547bbb69f
alexfdzs <alexfsale@gmail.com>
2022-02-10 13:42:36 +0100

    SDI-2122-203-2.1-Create Spring project.

---
+++ src/test/java/com/uniovi/sdi2122203spring/Sdi2122203SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122203spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122203SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
