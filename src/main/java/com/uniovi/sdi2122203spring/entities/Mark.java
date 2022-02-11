512bafc9ce8eca9d2283b09980959f850b8e63bf
alexfdzs <alexfsale@gmail.com>
2022-02-11 13:21:57 +0100

    SDI-2122-203-2.2-Trabajando con Controladores.

---
+++ src/main/java/com/uniovi/sdi2122203spring/entities/Mark.java
@@ -0,0 +1,31 @@
+package com.uniovi.sdi2122203spring.entities;
+
+public class Mark {
+    private String id;
+    private String email;
+    private String score;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
