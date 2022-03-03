0ea5adcb90a2caae7f76469eac08d503f90fc419
alexfdzs <alexfsale@gmail.com>
2022-03-03 11:08:18 +0100

    SDI-2122-203-4.6-SpringBoot-Paginación

--- src/main/java/com/uniovi/sdi2122203spring/CustomConfiguration.java
+++ src/main/java/com/uniovi/sdi2122203spring/CustomConfiguration.java
@@ -7,7 +7,11 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.data.domain.PageRequest;
 
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
@@ -30,5 +34,14 @@ public class CustomConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        int page = 0;
+        int size = 2;
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setFallbackPageable(PageRequest.of(page, size));
+        argumentResolvers.add(resolver);
     }
 }
